4c0d1ebd3d8afc0256cf163e6258989b1aa1038d
Flourish <flourishisnow@gmail.com>
2022-07-08 14:46:17 +0100

    Added image assets

---
+++ lib/src/utils/app_images.dart
@@ -0,0 +1,12 @@
+class AppImages {
+  static const String _path = 'assets/images/';
+  static const String profile = _path + 'profile.png';
+  static const String canFoods = _path + 'can_foods.png';
+  static const String catfish = _path + 'catfish.png';
+  static const String drinks = _path + 'drinks.png';
+  static const String pineapple = _path + 'pineapple.png';
+  static const String seaFoods = _path + 'sea_foods.png';
+  static const String watermelon = _path + 'watermelon.png';
+  static const String product1 = _path + 'product-1.png';
+  static const String product2 = _path + 'product-2.png';
+}
